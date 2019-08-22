f96862a55a24583a8ef8825aed4733efd69dde5b
= <yz120821884@163.com>
2019-08-22 09:56:13 +0800

    PPT下载完成

--- sjtiantang/pipelines.py
+++ sjtiantang/pipelines.py
@@ -14,7 +14,6 @@ class SjtiantangPipeline(object):
 
 class pptFilePipline(FilesPipeline):
     def file_path(self, request, response=None, info=None):
-        print('---------%s' % request.headers)
         # 获取文件下载路径
         item = request.meta['item']
         #  = urlparse(request.url).path
@@ -31,15 +30,14 @@ class pptFilePipline(FilesPipeline):
                 headers = {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
                 }
-
                 cookies = {
-                    'UM_distinctid': '16cb32a59591-0f34aedf1c94fb-c343162-1fa400-16cb32a595a50c',
-                    'CNZZDATA1277386113': '828220532-1566369342-%7C1566374777',
-                    'JSESSIONID': 'D68BB15C560D34A050F58A5F0A373EC2',
-                    'ksqw': 'fe288eb4aca6447bb59d66c12a4b4d1b',
-                    'lkj_au': '458c098f2f4b43b2a357e4c0116623aa',
-                    'nnj_ooi': 'a18d7c3579128123dd6d3765e13088deec2b653a34c488374b7488796f374887',
+                    'UM_distinctid': '16c199bd3c76e9-02cdd9ad00735a-c343162-384000-16c199bd3c836',
+                    'CNZZDATA1277386113': '1693883798-1563797052-%7C1566398760',
+                    'JSESSIONID': '557A746E3BA03E913268B3F61B6F2C2B',
+                    'ksqw': '0045a5659f07428e8eceb57e8dff1775',
+                    'lkj_au': '94de4d4f3d7d4907b8a833763d061840',
+                    'nnj_ooi': 'ad4535995666123c1b64967876b41f76556d78e7a22907ff4b7488796f374887',
                     'olp_ctt': 'e5d356896c390ee6'
                 }
-                yield scrapy.Request(fileUrl, meta={'item': item})
+                yield scrapy.Request(fileUrl,headers=headers,cookies=cookies, meta={'item': item})
         return item
--- sjtiantang/spiders/myPPTSpider.py
+++ sjtiantang/spiders/myPPTSpider.py
@@ -11,23 +11,20 @@ class pptSpider(scrapy.Spider):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
     }
-
     cookies = {
-        'UM_distinctid': '16cb32a59591-0f34aedf1c94fb-c343162-1fa400-16cb32a595a50c',
-        'CNZZDATA1277386113': '828220532-1566369342-%7C1566374777',
-        'JSESSIONID': 'D68BB15C560D34A050F58A5F0A373EC2',
-        'ksqw': 'fe288eb4aca6447bb59d66c12a4b4d1b',
-        'lkj_au': '458c098f2f4b43b2a357e4c0116623aa',
-        'nnj_ooi': 'a18d7c3579128123dd6d3765e13088deec2b653a34c488374b7488796f374887',
+        'UM_distinctid': '16c199bd3c76e9-02cdd9ad00735a-c343162-384000-16c199bd3c836',
+        'CNZZDATA1277386113': '1693883798-1563797052-%7C1566398760',
+        'JSESSIONID': '557A746E3BA03E913268B3F61B6F2C2B',
+        'ksqw': '0045a5659f07428e8eceb57e8dff1775',
+        'lkj_au': '94de4d4f3d7d4907b8a833763d061840',
+        'nnj_ooi': 'ad4535995666123c1b64967876b41f76556d78e7a22907ff4b7488796f374887',
         'olp_ctt': 'e5d356896c390ee6'
     }
-
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url,headers=self.headers,cookies=self.cookies,callback=self.parse)
 
     def parse(self, response):
-        '''
         divRowAList = response.css('div.row a')
         for divItem in divRowAList:
             downUrl = divItem.css('a::attr("href")').extract_first()
@@ -36,11 +33,10 @@ class pptSpider(scrapy.Spider):
             pptFileItem = testFileItem()
             pptFileItem['file_urls'] = [realUrl]
             pptFileItem['file_name'] = realName+".pptx"
-        yield pptFileItem
+            yield pptFileItem
 
         pageDivList = response.css('#cut-page a')
         for pageA in pageDivList:
             if pageA.css('i::attr("class")').extract_first() == 'fa fa-angle-right':
                 next_url = "https://www.sjtiantang.com/category/ppt?sort=default&page="+pageA.css('a::attr("data-num")').extract_first()
-                # yield scrapy.Request(url=next_url,headers=self.headers,callback=self.parse)
-        '''
+                yield scrapy.Request(url=next_url,headers=self.headers,cookies=self.cookies,callback=self.parse)
